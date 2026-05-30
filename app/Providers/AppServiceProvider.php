05ea23f69488932ef1747a131e5c9941182fa5c9
Arss <149451841+ariskiarr@users.noreply.github.com>
2026-05-30 11:11:13 +0700

    Refactor boot method to enforce HTTPS scheme

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,27 +7,13 @@ use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
-
-
-    public function boot(): void
-    {
-        if (app()->environment('production')) {
-        URL::forceScheme('https');
-        }
-    }
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        //
+        URL::forceScheme('https');
     }
 }
